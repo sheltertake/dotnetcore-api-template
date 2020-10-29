86a8733fd04e0b80b49f226188bbbcf87193f573
Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>
2020-10-29 18:39:08 +0100

    fix path - added results

--- src/FriendsApi.Host/Infra/Logging/CallContextEnricher.cs
+++ src/FriendsApi.Host/Infra/Logging/CallContextEnricher.cs
@@ -20,11 +20,11 @@ namespace FriendsApi.Host.Infra.Logging
 
                 logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("QueryString", _httpContextAccessor.HttpContext.Request.QueryString));
                 logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("SeverityLevel", logEvent.Level));
-                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("ServiceOperationName", logEvent.Properties["RequestPath"].ToString().Trim('"')));
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("RequestPath", logEvent.Properties["RequestPath"].ToString().Trim('"')));
 
                 logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("HostName", _httpContextAccessor.HttpContext.Request.Host));
                 if (logEvent.Properties.ContainsKey("SourceContext"))
-                    logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Logger", logEvent.Properties["SourceContext"].ToString().Trim('"')));
+                    logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("SourceContext", logEvent.Properties["SourceContext"].ToString().Trim('"')));
             }
         }
     }
